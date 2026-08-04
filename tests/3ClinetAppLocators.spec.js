1a96f7a43401776a46c2a2bb30c41ab2c7daf0e8
Mohammed Zeeshan <mzeeshan@solventum.com>
2026-08-04 19:29:06 +0530

    new Un and PW

--- tests/3ClinetAppLocators.spec.js
+++ tests/3ClinetAppLocators.spec.js
@@ -1,12 +1,14 @@
 import {test,expect}  from "@playwright/test"
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
 
 test("@Web ClientAppLocators", async({page})=>
 {
    
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
 
-    await page.locator("#userEmail").fill("anshika@gmail.com");
-    await page.locator("#userPassword").fill("Iamking@000");
+    await page.locator("#userEmail").fill(username);
+    await page.locator("#userPassword").fill(password);
     await page.locator("#login").click();
     //console.log (await page.locator(".card-body b").nth(0).textContent());
    // await page.waitForLoadState('networkidle');
